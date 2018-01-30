3261db4b5525fa64caaf5e6416c1159954550b57
Davis Whitehead <dwhitehead@eq-0366.lan>
2018-01-30 01:42:48 -0500

    working firestore example

--- app/containers/Feed.js
+++ app/containers/Feed.js
@@ -8,8 +8,16 @@ import {
 import ShayrStatusBar from '../components/StatusBar'
 import TitleBar from '../components/TitleBar'
 import List from '../components/List'
+import firebase from 'react-native-firebase'
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.posts = firebase.firestore().collection('posts');
+    console.log(this.posts);
+    console.log(this.posts.orderBy('created_at').limit(1).get());
+  }
+
   render() {
     return (
       <View style={styles.container}>
