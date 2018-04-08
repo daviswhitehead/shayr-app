b30280214c56b872866bf0bbf3ed88bc473ab04c
Alex <alex.trahey@gmail.com>
2018-04-08 18:27:59 -0400

    added login navigation and current user ID to db writes

--- app/Navigator.js
+++ app/Navigator.js
@@ -10,65 +10,12 @@ import {
   StyleSheet,
   TouchableOpacity,
   Image,
-  Button,
-  ActivityIndicator,
-  StatusBar,
-  AsyncStorage
 } from 'react-native';
 import Feed from './containers/Feed';
 import Queue from './containers/Queue';
-import { AccessToken, LoginManager } from 'react-native-fbsdk';
-import firebase from 'react-native-firebase'
+import LoginScreen from './containers/LoginScreen';
 
-class SignInScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Please sign in',
-  };
-
-
-  // Calling the following function will open the FB login dialogue:
-  _facebookLogin = async () => {
-    try {
-      const result = await LoginManager.logInWithReadPermissions(['public_profile', 'email']);
-
-      if (result.isCancelled) {
-        throw new Error('User cancelled request'); // Handle this however fits the flow of your app
-      }
-
-      console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`);
-
-      // get the access token
-      const data = await AccessToken.getCurrentAccessToken();
-
-      if (!data) {
-        throw new Error('Something went wrong obtaining the users access token'); // Handle this however fits the flow of your app
-      }
-
-      // create a new firebase credential with the token
-      const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
-
-      // login with credential
-      const currentUser = await firebase.auth().signInAndRetrieveDataWithCredential(credential);
-
-      console.info(JSON.stringify(currentUser.user.toJSON()))
-      this.props.navigation.navigate('App');
-
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-render() {
-  return (
-    <View>
-      <Button title="Login" onPress={this._facebookLogin} />
-    </View>
-  );
-
-  }
-}
-
-const AuthStack = StackNavigator({ SignIn: SignInScreen });
+const AuthStack = StackNavigator({ LoginScreen: LoginScreen });
 const AppStack = StackNavigator(
   {
     Feed: { screen: Feed },
