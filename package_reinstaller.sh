4f239ff83926b7c239816ce4d61875af960e546f
Davis Whitehead <whitehead.davis@gmail.com>
2018-09-11 20:55:16 -0400

    notifications working on android

---
+++ package_reinstaller.sh
@@ -0,0 +1,15 @@
+# https://facebook.github.io/react-native/docs/troubleshooting
+# fix any node_modules issues
+rm -rf node_modules/ && npm install
+
+# fix any pods issues
+cd ./ios
+rm -rf Pods/ && pod update
+cd ..
+
+# clear out builds
+rm -rf ios/build
+rm -rf android/build
+
+# link
+react-native link
