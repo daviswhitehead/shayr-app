70d6d7acb1d3d0b44a5e1e3b50d65d39de105ec9
Davis Whitehead <whitehead.davis@gmail.com>
2019-09-29 21:44:30 -0400

    got the how to post to appear in app :)

--- src/containers/Discover/Discover.tsx
+++ src/containers/Discover/Discover.tsx
@@ -1,4 +1,4 @@
-import { User, UsersPosts } from '@daviswhitehead/shayr-resources';
+import { Post, User, UsersPosts } from '@daviswhitehead/shayr-resources';
 import _ from 'lodash';
 import React, { PureComponent } from 'react';
 import { View } from 'react-native';
@@ -12,12 +12,17 @@ import PostCard from '../../components/PostCard';
 import SwipeCard from '../../components/SwipeCard';
 import withAdds from '../../higherOrderComponents/withAdds';
 import { getQuery, queryTypes } from '../../lib/FirebaseQueries';
+import { openURL } from '../../lib/Utils';
 import { selectAuthUserId } from '../../redux/auth/selectors';
-import { selectFlatListReadyDocuments } from '../../redux/documents/selectors';
+import {
+  selectDocumentFromId,
+  selectFlatListReadyDocuments
+} from '../../redux/documents/selectors';
 import { subscribeToFriendships } from '../../redux/friendships/actions';
 import { generateListKey } from '../../redux/lists/helpers';
 import { selectListItems, selectListMeta } from '../../redux/lists/selectors';
 import { subscribeNotificationTokenRefresh } from '../../redux/notifications/actions';
+import { getPost } from '../../redux/posts/actions';
 import { State } from '../../redux/Reducers';
 import { navigateToRoute } from '../../redux/routing/actions';
 import { subscribeToFriends, subscribeToUser } from '../../redux/users/actions';
@@ -30,12 +35,15 @@ import { loadUsersPosts } from '../../redux/usersPosts/actions';
 import colors from '../../styles/Colors';
 import styles from './styles';
 
+const SHAYR_ONBOARDING_POST_ID = 'SHAYR_HOW_TO';
+
 interface StateProps {
   authUser?: User;
   authUserId: string;
   friends: {
     [userId: string]: User;
   };
+  onboardingPost: Post;
   routing?: any; // routing state
   unreadNotificationsCount?: number;
   usersPostsListsMeta?: {
@@ -53,12 +61,14 @@ interface DispatchProps {
   subscribeToFriends: typeof subscribeToFriends;
   subscribeToFriendships: typeof subscribeToFriendships;
   subscribeNotificationTokenRefresh: typeof subscribeNotificationTokenRefresh;
+  getPost: typeof getPost;
 }
 
 interface OwnProps {}
 
 interface OwnState {
   isLoading: boolean;
+  isLoadingOnboardingPost: boolean;
 }
 
 interface NavigationParams {}
@@ -83,6 +93,11 @@ const mapStateToProps = (state: State) => {
       generateListKey(authUserId, queryTypes.USER_FRIENDS),
       'presentation'
     ),
+    onboardingPost: selectDocumentFromId(
+      state,
+      'posts',
+      SHAYR_ONBOARDING_POST_ID
+    ),
     routing: state.routing,
     unreadNotificationsCount: selectUserActionCount(
       state,
@@ -97,25 +112,26 @@ const mapStateToProps = (state: State) => {
         generateListKey(authUserId, queryTypes.USERS_POSTS_ALL)
       )
     },
-    usersPostsListsData: {
-      [generateListKey(
-        authUserId,
-        queryTypes.USERS_POSTS_ALL
-      )]: selectFlatListReadyDocuments(
-        state,
-        'usersPosts',
-        selectListItems(
-          state,
-          'usersPostsLists',
-          generateListKey(authUserId, queryTypes.USERS_POSTS_ALL)
-        ),
-        generateListKey(authUserId, queryTypes.USERS_POSTS_ALL),
-        {
-          sortKeys: ['createdAt'],
-          sortDirection: ['desc']
-        }
-      )
-    }
+    usersPostsListsData: []
+    // usersPostsListsData: {
+    //   [generateListKey(
+    //     authUserId,
+    //     queryTypes.USERS_POSTS_ALL
+    //   )]: selectFlatListReadyDocuments(
+    //     state,
+    //     'usersPosts',
+    //     selectListItems(
+    //       state,
+    //       'usersPostsLists',
+    //       generateListKey(authUserId, queryTypes.USERS_POSTS_ALL)
+    //     ),
+    //     generateListKey(authUserId, queryTypes.USERS_POSTS_ALL),
+    //     {
+    //       sortKeys: ['createdAt'],
+    //       sortDirection: ['desc']
+    //     }
+    //   )
+    // }
   };
 };
 
@@ -125,7 +141,8 @@ const mapDispatchToProps = {
   subscribeToUser,
   subscribeToFriends,
   subscribeToFriendships,
-  subscribeNotificationTokenRefresh
+  subscribeNotificationTokenRefresh,
+  getPost
 };
 
 class Discover extends PureComponent<Props, OwnState> {
@@ -135,7 +152,8 @@ class Discover extends PureComponent<Props, OwnState> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      isLoading: true
+      isLoading: true,
+      isLoadingOnboardingPost: false
     };
     this.subscriptions = [];
   }
@@ -196,6 +214,7 @@ class Discover extends PureComponent<Props, OwnState> {
 
   componentDidUpdate(prevProps: Props) {
     this.checkLoading();
+    this.loadOnboardingPost();
   }
 
   componentWillUnmount() {
@@ -215,6 +234,26 @@ class Discover extends PureComponent<Props, OwnState> {
       ].isLoaded
     ) {
       this.setState({ isLoading: false });
+    }
+  };
+
+  loadOnboardingPost = () => {
+    if (
+      this.state.isLoadingOnboardingPost &&
+      !_.isUndefined(this.props.onboardingPost)
+    ) {
+      this.setState({ isLoadingOnboardingPost: false });
+    }
+
+    if (
+      _.isEmpty(this.props.usersPostsListsData) &&
+      !_.isUndefined(this.props.usersPostsListsData) &&
+      _.isUndefined(this.props.onboardingPost) &&
+      !this.state.isLoadingOnboardingPost
+    ) {
+      this.setState({ isLoadingOnboardingPost: true }, () => {
+        this.props.getPost(SHAYR_ONBOARDING_POST_ID);
+      });
     }
   };
 
@@ -305,7 +344,34 @@ class Discover extends PureComponent<Props, OwnState> {
     );
   };
 
+  onPressOnboardingPost = ({ url }: { url: string }) => {
+    openURL(url);
+  };
+
+  renderOnboardingItem = ({ item }: { item: Post }) => {
+    console.log('item');
+    console.log(item);
+
+    return (
+      <PostCard
+        key={item._id}
+        isLoading={this.state.isLoadingOnboardingPost}
+        post={item}
+        onPressParameters={{
+          url: item.url
+        }}
+        onPress={this.onPressOnboardingPost}
+      />
+    );
+  };
+
   render() {
+    // console.log(`Discover - Render`);
+    // console.log('this.props');
+    // console.log(this.props);
+    // console.log('this.state');
+    // console.log(this.state);
+
     return (
       <View style={styles.screen}>
         <Header
@@ -327,47 +393,58 @@ class Discover extends PureComponent<Props, OwnState> {
             />
           }
         />
-        <List
-          data={
-            this.props.usersPostsListsData![
-              generateListKey(this.props.authUserId, queryTypes.USERS_POSTS_ALL)
-            ]
-          }
-          renderItem={this.renderItem}
-          onEndReached={this.paginateList}
-          onRefresh={this.refreshList}
-          isLoading={this.state.isLoading}
-          isRefreshing={
-            this.state.isLoading
-              ? false
-              : this.props.usersPostsListsMeta![
-                  generateListKey(
-                    this.props.authUserId,
-                    queryTypes.USERS_POSTS_ALL
-                  )
-                ].isRefreshing
-          }
-          isPaginating={
-            this.state.isLoading
-              ? false
-              : this.props.usersPostsListsMeta![
-                  generateListKey(
-                    this.props.authUserId,
-                    queryTypes.USERS_POSTS_ALL
-                  )
-                ].isLoading
-          }
-          isLoadedAll={
-            this.state.isLoading
-              ? false
-              : this.props.usersPostsListsMeta![
-                  generateListKey(
-                    this.props.authUserId,
-                    queryTypes.USERS_POSTS_ALL
-                  )
-                ].isLoadedAll
-          }
-        />
+        {this.props.onboardingPost ? (
+          <List
+            data={[this.props.onboardingPost]}
+            renderItem={this.renderOnboardingItem}
+            isLoading={this.state.isLoadingOnboardingPost}
+          />
+        ) : (
+          <List
+            data={
+              this.props.usersPostsListsData![
+                generateListKey(
+                  this.props.authUserId,
+                  queryTypes.USERS_POSTS_ALL
+                )
+              ]
+            }
+            renderItem={this.renderItem}
+            onEndReached={this.paginateList}
+            onRefresh={this.refreshList}
+            isLoading={this.state.isLoading}
+            isRefreshing={
+              this.state.isLoading
+                ? false
+                : this.props.usersPostsListsMeta![
+                    generateListKey(
+                      this.props.authUserId,
+                      queryTypes.USERS_POSTS_ALL
+                    )
+                  ].isRefreshing
+            }
+            isPaginating={
+              this.state.isLoading
+                ? false
+                : this.props.usersPostsListsMeta![
+                    generateListKey(
+                      this.props.authUserId,
+                      queryTypes.USERS_POSTS_ALL
+                    )
+                  ].isLoading
+            }
+            isLoadedAll={
+              this.state.isLoading
+                ? false
+                : this.props.usersPostsListsMeta![
+                    generateListKey(
+                      this.props.authUserId,
+                      queryTypes.USERS_POSTS_ALL
+                    )
+                  ].isLoadedAll
+            }
+          />
+        )}
       </View>
     );
   }
